f027ec89735ee7b3fde7195df98ccdb09336668d
Jay's Script's <116266395+Jays-Scripts@users.noreply.github.com>
2024-01-02 18:45:48 +0000

    v1.1 :0

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "cerulean"
+game "gta5"
+author "Jays Scripts"
+description "Spawn animations (Definitely NOT inspired)
+shared_scripts {
+  'config.lua',
+}
+
+client_scripts {
+  "client/**"
+}

14451b3fed8e4689586e5bd52d1082cdb5c5845f
Jay's Script's <116266395+Jays-Scripts@users.noreply.github.com>
2024-01-02 18:44:31 +0000

    v.1.1 ;)

--- spawnanim.lua
+++ spawnanim.lua
@@ -1,68 +1,68 @@
-local randomEmotes = {"stretch3","stretch4","prone"}
+local randomEmotes = Config.RandomEmotes
 local randomIndex = math.random(1, #randomEmotes)
 local selectedEmote = randomEmotes[randomIndex]
 
-local function initialLoadingIn()
+local function initialLoadingIn(coordsWhereYouSpawn)
   exports.scully_emotemenu:cancelEmote()
   SetEntityVisible(cache.ped, false)
   DoScreenFadeOut(1)
   local startingCam = CreateCam("DEFAULT_SCRIPTED_CAMERA", true)
-  local currentCoords = GetEntityCoords(PlayerPedId()) -- Eventually callback from server the required coords
-  SetCamCoord(startingCam, currentCoords.x + 1, currentCoords.y + 1, currentCoords.z + 500) -- Make this get ground coords and do maths
+  SetCamCoord(startingCam, coordsWhereYouSpawn.x + 1, coordsWhereYouSpawn.y + 1, coordsWhereYouSpawn.z + 500) -- Make this get ground coords and do maths
   SetCamFov(startingCam, 150.0)
   RenderScriptCams(true, true, 3500, true, true)
   Wait(1000)
-  PointCamAtCoord(startingCam, currentCoords.x, currentCoords.y, currentCoords.z-0.1)
+  PointCamAtCoord(startingCam, coordsWhereYouSpawn.x, coordsWhereYouSpawn.y, coordsWhereYouSpawn.z-0.1)
   Wait(1000)
   DoScreenFadeIn(2000)
 end
 
-local function stoppingCam1stStage(durationToRender)
+local function destroyAllCameras(durationToRender)
   SetNuiFocus(false, false)
   RenderScriptCams(false, true, durationToRender, true, true)
   DestroyAllCams()
 end
 
-local function startSpawnCamera()
-  initialLoadingIn()
-
+local function startSpawnCamera(coordsWhereYouSpawn)
+  initialLoadingIn(coordsWhereYouSpawn)
   Wait(4500)
-
-  stoppingCam1stStage(7500)
-
+  destroyAllCameras(7500)
   SetEntityVisible(cache.ped, true)
   exports.scully_emotemenu:playEmoteByCommand(selectedEmote)
-  print("Played emote: ",selectedEmote)
-
+  if Config.Debug then
+    print("Played emote: ",selectedEmote)
+  end
   Wait(7000)
-  
   local startingCam = CreateCam("DEFAULT_SCRIPTED_CAMERA", true)
-  local currentCoords = GetEntityCoords(PlayerPedId()) -- Eventually callback from server the required coords
   if selectedEmote == "prone" then
-    SetCamCoord(startingCam, currentCoords.x + 1, currentCoords.y - 2, currentCoords.z - 0.5) -- Make this get ground coords and do maths
+    SetCamCoord(startingCam, coordsWhereYouSpawn.x + 1, coordsWhereYouSpawn.y - 2, coordsWhereYouSpawn.z - 0.5) -- Make this get ground coords and do maths
   else
-    SetCamCoord(startingCam, currentCoords.x + 1, currentCoords.y + 2, currentCoords.z + 0.5) -- Make this get ground coords and do maths
+    SetCamCoord(startingCam, coordsWhereYouSpawn.x + 1, coordsWhereYouSpawn.y + 2, coordsWhereYouSpawn.z + 0.5) -- Make this get ground coords and do maths
   end
     SetCamFov(startingCam, 80.0)
     RenderScriptCams(true, true, 5500, true, true)
-    PointCamAtCoord(startingCam, currentCoords.x, currentCoords.y, currentCoords.z-0.4)
-  
-  Wait(6000)
-
-  stoppingCam1stStage(5000)
-  Wait(2000)
-
-  exports.scully_emotemenu:cancelEmote()
+    PointCamAtCoord(startingCam, coordsWhereYouSpawn.x, coordsWhereYouSpawn.y, coordsWhereYouSpawn.z-0.4)
+    Wait(6000)
+    destroyAllCameras(5000)
+    Wait(2000)
+    exports.scully_emotemenu:cancelEmote()
 end
 
-local function startToStandUpPlease()
+-- local function startToStandUpPlease()  -- adding later on
+-- end
 
-end
-
-RegisterCommand("startstartanim", function()
-  startSpawnCamera()
+RegisterNetEvent("jays-spawn-anim:doSpawnAnimationAndSpawnIn", function(selectedSpawnCoords)
+  startSpawnCamera(selectedSpawnCoords)
 end)
 
-RegisterCommand("stopstartanim", function()
-  stoppingCam1stStage()
+CreateThread(function()
+  if Config.Debug then
+    RegisterCommand("startstartanim", function()
+      local selectedSpawnCoords = GetEntityCoords(PlayerPedId()) -- Eventually callback from server the required coords
+      TriggerEvent("jays-spawn-anim:doSpawnAnimationAndSpawnIn", selectedSpawnCoords)
+    end)
+
+    RegisterCommand("stopstartanim", function()
+      destroyAllCameras()
+    end)
+  end
 end)

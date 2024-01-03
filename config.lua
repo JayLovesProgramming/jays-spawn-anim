4bd35e8b231317c97453f5330f22b2f7f420fd19
Jay's Script's <116266395+Jays-Scripts@users.noreply.github.com>
2024-01-03 23:07:27 +0000

    added tips

--- config.lua
+++ config.lua
@@ -1,23 +1,22 @@
-Config = {}
+Config = {} -- Don't delete or change this line
 
-Config.Debug = true
+Config.Debug = false -- Set to true if you are debugging, false if you are not
 
-Config.DebugAdvanced = false
+Config.DebugAdvanced = false -- Only use this if you feel like you can move the player while loading in / doing animation (It should print Disabling controls when in animation)
 
-Config.HowHighInTheSky = 55  -- This determines how high in the sky you first spawn (EXAMPLE: 55 = 5500(GTA distance) in the sky)
+Config.HowHighInTheSky = 55  -- This determines how high in the sky you first spawn (EXAMPLE: 55 = 5500 (GTA distance) in the sky)
 
-Config.WaitTimeToRenderCamera = 7500
+Config.WaitTimeToRenderCamera = 7500 -- This converts to 7.5 seconds
 
-Config.HowLongItTakesToZoomInToPlayer = 5000
+Config.HowLongItTakesToZoomInToPlayer = 5000 -- This converts to 5 seconds
 
-Config.CamFov = 21.0
+Config.CamFov = 21.0 -- Camera FOV for standard animations
 
-Config.CamFovProne = 27.0
+Config.CamFovProne = 27.0 -- Camera FOV for prone animation if you are using emote "prone"
 
-Config.RandomEmotes = { -- Adjust these emotes as you like
+Config.RandomEmotes = { -- Adjust these emotes as you like (ADD OR REMOVE AS MANY AS YOU LIKE)
   "stretch",
   "stretch2",
   "stretch4",
-  "prone"
+  "prone" -- Uncomment this line if you don't want to use prone emote
 }
-

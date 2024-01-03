9811fe73ca8e561d5bf68ff27d204ecf4a623bcf
Jay's Script's <116266395+Jays-Scripts@users.noreply.github.com>
2024-01-03 21:54:49 +0000

    updated config
    still needs work

--- config.lua
+++ config.lua
@@ -1,9 +1,22 @@
 Config = {}
 
-Config.Debug = false
+Config.Debug = true
+
+Config.DebugAdvanced = false
+
+Config.HowHighInTheSky = 55  -- This determines how high in the sky you first spawn (EXAMPLE: 55 = 5500(GTA distance) in the sky)
+
+Config.WaitTimeToRenderCamera = 7500
+
+Config.HowLongItTakesToZoomInToPlayer = 5000
+
+Config.CamFov = 21.0
+
+Config.CamFovProne = 27.0
 
 Config.RandomEmotes = { -- Adjust these emotes as you like
-  "stretch3",
+  "stretch",
+  "stretch2",
   "stretch4",
   "prone"
 }
